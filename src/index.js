517e562bd9cb49cf803e26de72072446f321da4c
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-10-25 20:54:56 -0700

    move code from sidebar.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
 import './style.css'
+
+
+// home button
+
+// today button
+
+// upcoming button
+
+// projects -> list of all projects
+
+//  add project
+
+// following for MVC https://www.taniarascia.com/javascript-mvc-todo-app/
+
+class Model {
+  constructor() {
+    this.todos = [
+      {id: 1, text: 'Run a marathon', complete: false},
+      {id: 2, text: 'Plant a garden', complete: false},
+    ]
+  }
+
+  addTodo(todoText) {
+    const todo = {
+      id: this.todo.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
+      text: todoText,
+      complete: false,
+    }
+
+    this.todos.push(todo)
+  }
+
+  editTodo(id, updatedText) {
+    this.todos = this.todos.map((todo) => 
+    todo.id === id ? {id: todo.id, text: updatedText, complete: todo.complete} : todo,
+    )
+  }
+
+  deleteTodo(id) {
+    this.todos = this.todos.filter((todo) => todo.id !== id)
+  }
+
+  toggleTodo(id) {
+    this.todos = this.todos.map((todo) =>
+    todo.id === id ? {id: todo.id, text: todo.text, complete: !todo.complete} : todo
+    )
+  }
+}
+
+class View {
+  constructor() {}
+}
+
+class Controller {
+  constructor(model, view) {
+    this.model = model
+    this.view = view
+  }
+}
+
+const app = new Controller(new Model(), new View())
