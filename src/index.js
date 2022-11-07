f59a489f5ec3954562b0cd8a9dcdf37f0636622b
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-11-07 11:38:49 -0800

    style new project container and button

--- src/index.js
+++ src/index.js
@@ -387,14 +387,14 @@ class View {
 
 
     // project name form
-    this.projectNameForm = this.createElem('form', 'hidden invisible opacity-0 transition-opacity duration-150 ease-in-out absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none divide-y divide-gray-100 p-4')
+    this.projectNameForm = this.createElem('form', 'hidden invisible opacity-0 transition-opacity duration-150 ease-in-out absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none divide-y divide-gray-100 p-4 dark:border-gray-100 dark:bg-gray-800 dark:divide-gray-600')
 
-    this.projectName = this.createElem('input', 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5')
+    this.projectName = this.createElem('input', 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500')
     this.projectName.type = 'text'
     this.projectName.placeholder = 'e.g., Computer Science'
     this.projectName.name = 'projectName'
 
-    this.projectNameLabel = this.createElem('label', 'block mb-2 text-sm font-medium text-gray-900')
+    this.projectNameLabel = this.createElem('label', 'block mb-2 text-sm font-medium text-gray-900 dark:text-gray-100')
     this.projectNameLabel.textContent = 'Name your new project'
     this.projectNameContainer = this.createElem('div', 'mb-2')
     this.projectNameContainer.append(this.projectNameLabel, this.projectName)
